e830181580297445f68fc29b093f95f8d03fd27b
Brack-Wang <wfr2099@gmail.com>
2023-04-21 21:13:50 -0500

    new

--- brute_force.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Find at most `max_output_count` occurrences of pattern in text, and store
-// them in the array `output`.
-// This brute force solution is only used to check correctness.
-int brute_force_search(
-    const char *text, int text_length, const char *pattern, int pattern_length,
-    int *output, int max_output_cnt, int *fail
-);
