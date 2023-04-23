36fa83313687d4dc9cc8d89b3870e81140e7c88c
xye16 <xye16@illinois.edu>
2023-04-23 17:49:03 -0500

    Change the signature of brute_force_search to make it consistent with KMP_search

--- brute_force.cpp
+++ brute_force.cpp
@@ -1,18 +1,14 @@
 #include "brute_force.hpp"
-
-// Get arr[idx], where arr is the compact form of the gene sequence.
-static inline char get(const char *arr, int idx) {
-    return (arr[idx>>2] >> ((idx & 0x3) << 1)) & 0x3;
-}
+#include "char_compress.hpp"
 
 int brute_force_search(
-    const char *text, int text_length, const char *pattern, int pattern_length,
-    int *output, int max_output_cnt, int *fail
+    const char *text, int text_length, const char *pattern, int16_t pattern_length,
+    int16_t *output, int max_output_cnt, int16_t *fail
 ) {
     int output_cnt = 0;
     for (int i = 0; i < text_length - pattern_length; i++) {
         bool match = true;
-        for (int j = 0; j < pattern_length; j++) {
+        for (int16_t j = 0; j < pattern_length; j++) {
             if (get(text, i+j) != get(pattern, j)) {
                 match = false;
                 break;
--- brute_force.hpp
+++ brute_force.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <cstdint>
+
 // Find at most `max_output_count` occurrences of pattern in text, and store
 // them in the array `output`.
 // This brute force solution is only used to check correctness.
 int brute_force_search(
-    const char *text, int text_length, const char *pattern, int pattern_length,
-    int *output, int max_output_cnt, int *fail
+    const char *text, int text_length, const char *pattern, int16_t pattern_length,
+    int16_t *output, int max_output_cnt, int16_t *fail
 );
