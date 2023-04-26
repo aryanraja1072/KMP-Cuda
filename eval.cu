1def55505623a29740ac0250b9e14c916a76e61e
aryanraja1072 <aryanraja1072@wirelessprv-10-195-215-11.near.illinois.edu>
2023-04-26 16:35:36 -0500

    Add constant memory usage

--- eval.cu
+++ eval.cu
@@ -13,43 +13,55 @@
 
 using StringMatchingFunction = decltype(brute_force_search);
 
-void generate_random_data(char *data, int length) {
+void generate_random_data(char *data, int length)
+{
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<char> distrib(-128, 127);
-    for (int i = 0; 4*i < length; i++) {
+    for (int i = 0; 4 * i < length; i++)
+    {
         data[i] = distrib(gen);
     }
 }
 
-void print_text_or_pattern(const std::string &text_or_pattern, int length) {
-    for (int i = 0; i < length; i++) {
+void print_text_or_pattern(const std::string &text_or_pattern, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
         char char_compressed = get(text_or_pattern.data(), i);
-        if (char_compressed == static_cast<char>(Gene::A)) {
+        if (char_compressed == static_cast<char>(Gene::A))
+        {
             printf("A");
         }
-        else if (char_compressed == static_cast<char>(Gene::T)) {
+        else if (char_compressed == static_cast<char>(Gene::T))
+        {
             printf("T");
         }
-        else if (char_compressed == static_cast<char>(Gene::C)) {
+        else if (char_compressed == static_cast<char>(Gene::C))
+        {
             printf("C");
         }
-        else if (char_compressed == static_cast<char>(Gene::G)) {
+        else if (char_compressed == static_cast<char>(Gene::G))
+        {
             printf("G");
         }
     }
     printf(" (hex = ");
-    for (int i = 0; 4*i < length; i++) {
+    for (int i = 0; 4 * i < length; i++)
+    {
         printf("%02hhx", text_or_pattern[i]);
     }
     printf(")");
 }
 
-void print_output(std::vector<int> &output, int output_cnt) {
+void print_output(std::vector<int> &output, int output_cnt)
+{
     printf("%d matches found\n", output_cnt);
-    if (output_cnt > 0) {
+    if (output_cnt > 0)
+    {
         printf("matching at: ");
-        for (int i = 0; i < output_cnt; i++) {
+        for (int i = 0; i < output_cnt; i++)
+        {
             printf("%d ", output[i]);
         }
         printf("\n");
@@ -60,17 +72,18 @@ void print_output(std::vector<int> &output, int output_cnt) {
 int compare_result(
     const std::vector<int> &output, int cnt,
     const std::vector<int> &ref_output, int ref_cnt,
-    int max_output_cnt
-) {
-    if (cnt != ref_cnt) {
+    int max_output_cnt)
+{
+    if (cnt != ref_cnt)
+    {
         printf("Pattern count mismatch! %d (tested) != %d (ref)!\n", cnt, ref_cnt);
         return -1;
     }
-    if (cnt == max_output_cnt) {
+    if (cnt == max_output_cnt)
+    {
         printf(
             "Warning: output limit is reached! The GPU and CPU algorithms may produce different result.\n"
-            "Results will not be compared this time. You should set a larger max_output_cnt and try again.\n"
-        );
+            "Results will not be compared this time. You should set a larger max_output_cnt and try again.\n");
         return 0;
     }
 
@@ -78,8 +91,10 @@ int compare_result(
     std::sort(sorted_output.begin(), sorted_output.begin() + cnt);
     std::vector<int> sorted_ref_output = ref_output;
     std::sort(sorted_ref_output.begin(), sorted_ref_output.begin() + ref_cnt);
-    for (int i = 0; i < cnt; i++) {
-        if (sorted_output[i] != sorted_ref_output[i]) {
+    for (int i = 0; i < cnt; i++)
+    {
+        if (sorted_output[i] != sorted_ref_output[i])
+        {
             printf("Mismatch at the %d-th occurrence, %d (tested) != %d (ref)!\n", i, sorted_output[i], sorted_ref_output[i]);
             return -1;
         }
@@ -93,10 +108,11 @@ int eval(
     const std::string &text, int text_length,
     std::string &pattern, int16_t pattern_length,
     StringMatchingFunction search_function,
-    StringMatchingFunction reference_function=nullptr,
-    bool verbose=false, int max_output_cnt=100
-) {
-    if (verbose) {
+    StringMatchingFunction reference_function = nullptr,
+    bool verbose = false, int max_output_cnt = 100)
+{
+    if (verbose)
+    {
         printf("text = ");
         print_text_or_pattern(text, text_length);
         printf("\n");
@@ -112,30 +128,31 @@ int eval(
     auto start = std::chrono::steady_clock::now();
     int cnt = search_function(
         text.data(), text_length, pattern.data(), pattern_length,
-        output.data(), output.size(), fail.data()
-    );
+        output.data(), output.size(), fail.data());
     auto end = std::chrono::steady_clock::now();
     std::chrono::duration<double, std::milli> diff = end - start;
     printf("Search function takes %lf ms.\n", diff.count());
 
-    if (verbose) {
+    if (verbose)
+    {
         printf("test function output: ");
         print_output(output, cnt);
     }
 
-    if (reference_function != nullptr) {
+    if (reference_function != nullptr)
+    {
         std::vector<int> ref_output(max_output_cnt);
 
         auto start = std::chrono::steady_clock::now();
         int ref_cnt = reference_function(
             text.data(), text_length, pattern.data(), pattern_length,
-            ref_output.data(), ref_output.size(), fail.data()
-        );
+            ref_output.data(), ref_output.size(), fail.data());
         auto end = std::chrono::steady_clock::now();
         std::chrono::duration<double, std::milli> diff = end - start;
         printf("Reference function takes %lf ms.\n", diff.count());
 
-        if (verbose) {
+        if (verbose)
+        {
             printf("reference function output: ");
             print_output(ref_output, ref_cnt);
         }
@@ -149,20 +166,21 @@ int eval(
 int eval_with_random_input(
     int text_length, int pattern_length,
     StringMatchingFunction search_function,
-    StringMatchingFunction reference_function=nullptr,
-    bool verbose=false, int run_cnt=1, int max_output_cnt=100
-) {
-    std::string text((text_length-1)/4+1, '\0');
-    std::string pattern((pattern_length-1)/4+1, '\0');
-    for (int i = 0; i < run_cnt; i++) {
+    StringMatchingFunction reference_function = nullptr,
+    bool verbose = false, int run_cnt = 1, int max_output_cnt = 100)
+{
+    std::string text((text_length - 1) / 4 + 1, '\0');
+    std::string pattern((pattern_length - 1) / 4 + 1, '\0');
+    for (int i = 0; i < run_cnt; i++)
+    {
         generate_random_data(&text[0], text_length);
         generate_random_data(&pattern[0], pattern_length);
 
         int retval = eval(
             text, text_length, pattern, pattern_length,
-            search_function, reference_function, verbose, max_output_cnt
-        );
-        if (retval == -1) {
+            search_function, reference_function, verbose, max_output_cnt);
+        if (retval == -1)
+        {
             return -1;
         }
     }
@@ -173,24 +191,25 @@ int eval_with_random_input(
 int eval_with_string_data(
     const char *text, const char *pattern,
     StringMatchingFunction search_function,
-    StringMatchingFunction reference_function=nullptr,
-    bool verbose=false, int run_cnt=1, int max_output_cnt=100
-) {
+    StringMatchingFunction reference_function = nullptr,
+    bool verbose = false, int run_cnt = 1, int max_output_cnt = 100)
+{
     int text_length = strlen(text);
     int pattern_length = strlen(pattern);
-    std::string compressed_text((text_length-1)/4+1, '\0');
-    std::string compressed_pattern((pattern_length-1)/4+1, '\0');
+    std::string compressed_text((text_length - 1) / 4 + 1, '\0');
+    std::string compressed_pattern((pattern_length - 1) / 4 + 1, '\0');
 
     char_compress(text, text_length, &compressed_text[0]);
     char_compress(pattern, pattern_length, &compressed_pattern[0]);
 
-    for (int i =0; i < run_cnt; i++) {
+    for (int i = 0; i < run_cnt; i++)
+    {
         int retval = eval(
             compressed_text, text_length,
             compressed_pattern, pattern_length,
-            search_function, reference_function, verbose, max_output_cnt
-        );
-        if (retval == -1) {
+            search_function, reference_function, verbose, max_output_cnt);
+        if (retval == -1)
+        {
             return -1;
         }
     }
@@ -202,48 +221,50 @@ int eval_with_string_data(
 int eval_with_dataset_file(
     const char *filename, int text_length, int pattern_length,
     StringMatchingFunction search_function,
-    StringMatchingFunction reference_function=nullptr,
-    bool verbose=false, int run_cnt=1, int max_output_cnt=100
-) {
-    std::string compressed_text((text_length-1)/4+1, '\0');
+    StringMatchingFunction reference_function = nullptr,
+    bool verbose = false, int run_cnt = 1, int max_output_cnt = 100)
+{
+    std::string compressed_text((text_length - 1) / 4 + 1, '\0');
 
     std::ifstream in_file(filename, std::ios::binary);
-    if (!in_file) {
+    if (!in_file)
+    {
         printf("Error opening file %s\n", filename);
         return 1;
     }
     in_file.read(&compressed_text[0], compressed_text.size());
 
     int actual_bytes_read = in_file.gcount();
-    if (actual_bytes_read < compressed_text.size()) {
+    if (actual_bytes_read < compressed_text.size())
+    {
         printf(
             "Not enough bytes in the data file, expected %d bytes but only got %d bytes\n",
-            static_cast<int>(compressed_text.size()), actual_bytes_read
-        );
+            static_cast<int>(compressed_text.size()), actual_bytes_read);
         return 1;
     }
 
-    std::string compressed_pattern((pattern_length-1)/4+1, '\0');
-    for (int i = 0; i < run_cnt; i++) {
+    std::string compressed_pattern((pattern_length - 1) / 4 + 1, '\0');
+    for (int i = 0; i < run_cnt; i++)
+    {
         generate_random_data(&compressed_pattern[0], pattern_length);
 
         int retval = eval(
             compressed_text, text_length,
             compressed_pattern, pattern_length,
-            search_function, reference_function, verbose, max_output_cnt
-        );
-        if (retval == -1) {
+            search_function, reference_function, verbose, max_output_cnt);
+        if (retval == -1)
+        {
             return -1;
         }
     }
     return 0;
 }
 
-int main() {
+int main()
+{
     // Note: for timing GPU kernel, the first few runs should be ignored since
     // there is JIT compiling overhead.
     eval_with_dataset_file(
-        "your_data_file.bin", 1000000000, 13, KMP_search_shmem, nullptr, /*verbose*/false, 6
-    );
+        "data.bin", 1000000000, 13, KMP_search_shmem, nullptr, /*verbose*/ false, 6);
     return 0;
 }
--- kmp_shmem.cu
+++ kmp_shmem.cu
@@ -10,52 +10,68 @@ static constexpr int output_cache_size_per_block = 20;
 
 static constexpr int block_size = 512;
 
-static __host__ __device__ int ceil_div(int x, int y) {
+static const int MAX_PATTERN_LENGTH = 100;
+
+__constant__ int16_t constant_fail[MAX_PATTERN_LENGTH + 1];
+
+static __host__ __device__ int ceil_div(int x, int y)
+{
     return (x - 1) / y + 1;
 }
 
-static int round_up_to_multiple(int x, int y) {
+static int round_up_to_multiple(int x, int y)
+{
     return ceil_div(x, y) * y;
 }
 
 // Get arr[idx], where arr is the compact form of the gene sequence.
-template<class IdxType>
-static __host__ __device__ inline char get(const char *arr, IdxType idx) {
-    return (arr[idx>>2] >> ((idx & 0x3) << 1)) & 0x3;
+template <class IdxType>
+static __host__ __device__ inline char get(const char *arr, IdxType idx)
+{
+    return (arr[idx >> 2] >> ((idx & 0x3) << 1)) & 0x3;
 }
 
 __device__ void KMP_search(
     const char *text, int text_start, int text_end,
     const char *pattern, int16_t pattern_length,
     int *shared_output, int *shared_output_cnt,
-    int *global_output, int *global_output_cnt, int max_output_cnt,
-    int16_t *fail
-) {
+    int *global_output, int *global_output_cnt, int max_output_cnt)
+{
     int i = text_start;
     int16_t j = 0;
-    while (i < text_end) {
-        if (get(text, i) == get(pattern, j)) {
-            i++; j++;
-            if (j == pattern_length) {
+    while (i < text_end)
+    {
+        if (get(text, i) == get(pattern, j))
+        {
+            i++;
+            j++;
+            if (j == pattern_length)
+            {
                 // Occurrence found. Try write the output to the shared memory.
                 int output_idx = atomicAdd(shared_output_cnt, 1);
-                if (output_idx < output_cache_size_per_block) {
+                if (output_idx < output_cache_size_per_block)
+                {
                     shared_output[output_idx] = i - j;
                 }
-                else {
+                else
+                {
                     // Write to global memory.
                     int output_idx = atomicAdd(global_output_cnt, 1);
-                    if (output_idx < max_output_cnt) {
+                    if (output_idx < max_output_cnt)
+                    {
                         global_output[output_idx] = i - j;
                     }
-                    else break;
+                    else
+                        break;
                 }
-                j = fail[j];
+                j = constant_fail[j];
             }
         }
-        else {
-            j = fail[j];
-            if (j < 0) {
+        else
+        {
+            j = constant_fail[j];
+            if (j < 0)
+            {
                 i++, j++;
             }
         }
@@ -64,30 +80,15 @@ __device__ void KMP_search(
 
 __global__ void KMP_search_shmem_kernel(
     const char *text, int text_length, const char *pattern, int16_t pattern_length,
-    int *output, int *output_cnt, int max_output_cnt, int16_t *fail
-) {
+    int *output, int *output_cnt, int max_output_cnt)
+{
     extern __shared__ char shared_memory[];
     int *shared_output = reinterpret_cast<int *>(shared_memory);
     int *shared_output_cnt = reinterpret_cast<int *>(
-        shared_memory
-        + sizeof(int) * output_cache_size_per_block
-    );
+        shared_memory + sizeof(int) * output_cache_size_per_block);
     int *shared_global_output_start = reinterpret_cast<int *>(
-        shared_memory
-        + sizeof(int) * output_cache_size_per_block
-        + sizeof(int)
-    );
-    int16_t *shared_fail = reinterpret_cast<int16_t *>(
-        shared_memory
-        + sizeof(int) * output_cache_size_per_block
-        + 2 * sizeof(int)
-    );
-    char *shared_pattern = (
-        shared_memory
-        + sizeof(int) * output_cache_size_per_block
-        + 2 * sizeof(int)
-        + sizeof(int16_t) * (pattern_length + 1)
-    );
+        shared_memory + sizeof(int) * output_cache_size_per_block + sizeof(int));
+    char *shared_pattern = (shared_memory + sizeof(int) * output_cache_size_per_block + 2 * sizeof(int) + sizeof(int16_t) * (pattern_length + 1));
 
     int global_index = blockIdx.x * blockDim.x + threadIdx.x;
     int match_start = (match_length_per_thread - (pattern_length - 1)) * global_index;
@@ -96,26 +97,24 @@ __global__ void KMP_search_shmem_kernel(
     match_end = min(match_end, text_length);
 
     // Initialize shared memory.
-    for (int i = threadIdx.x; i < pattern_length; i += blockDim.x) {
+    for (int i = threadIdx.x; i < pattern_length; i += blockDim.x)
+    {
         shared_pattern[i] = pattern[i];
     }
-    for (int i = threadIdx.x; i <= pattern_length; i += blockDim.x) {
-        shared_fail[i] = fail[i];
-    }
-    if (threadIdx.x == 0) {
+    if (threadIdx.x == 0)
+    {
         *shared_output_cnt = 0;
     }
     __syncthreads();
 
     KMP_search(
         text, match_start, match_end, shared_pattern, pattern_length,
-        shared_output, shared_output_cnt, output, output_cnt, max_output_cnt,
-        shared_fail
-    );
+        shared_output, shared_output_cnt, output, output_cnt, max_output_cnt);
 
     // Write the outputs to the global memory.
     int block_output_cnt = min(*shared_output_cnt, output_cache_size_per_block);
-    if (threadIdx.x == 0) {
+    if (threadIdx.x == 0)
+    {
         *shared_global_output_start = atomicAdd(output_cnt, block_output_cnt);
     }
     __syncthreads();
@@ -124,17 +123,18 @@ __global__ void KMP_search_shmem_kernel(
     for (
         int i = threadIdx.x;
         i < block_output_cnt && i + global_output_start < max_output_cnt;
-        i += blockDim.x
-    ) {
+        i += blockDim.x)
+    {
         output[i + global_output_start] = shared_output[i];
     }
 }
 
 int KMP_search_shmem(
     const char *text, int text_length, const char *pattern, int16_t pattern_length,
-    int *output, int max_output_cnt, int16_t *fail
-) {
-    if (match_length_per_thread <= pattern_length) {
+    int *output, int max_output_cnt, int16_t *fail)
+{
+    if (match_length_per_thread <= pattern_length)
+    {
         LOG(error, "match_length_per_thread should be larger than pattern_length");
         exit(1);
     }
@@ -150,12 +150,11 @@ int KMP_search_shmem(
     char *text_device;
     char *pattern_device;
     int *output_device;
-    int16_t *fail_device;
+
     int *output_cnt_device;
     THROW_IF_ERROR(cudaMalloc((void **)&text_device, text_size));
     THROW_IF_ERROR(cudaMalloc((void **)&pattern_device, pattern_size));
     THROW_IF_ERROR(cudaMalloc((void **)&output_device, output_size));
-    THROW_IF_ERROR(cudaMalloc((void **)&fail_device, fail_size));
     THROW_IF_ERROR(cudaMalloc((void **)&output_cnt_device, sizeof(int)));
     timer_stop();
 
@@ -166,36 +165,32 @@ int KMP_search_shmem(
     timer_start("Copying inputs to the GPU");
     THROW_IF_ERROR(cudaMemcpy(text_device, text, text_size, cudaMemcpyHostToDevice));
     THROW_IF_ERROR(cudaMemcpy(pattern_device, pattern, pattern_size, cudaMemcpyHostToDevice));
-    THROW_IF_ERROR(cudaMemcpy(fail_device, fail, fail_size, cudaMemcpyHostToDevice));
+    THROW_IF_ERROR(cudaMemcpyToSymbol(constant_fail, fail, fail_size));
     THROW_IF_ERROR(cudaMemset(output_cnt_device, 0, sizeof(int)));
     timer_stop();
 
     // Prepare to launch the kernel.
     int shared_memory_size =
-        pattern_size + fail_size
-        + sizeof(int) * output_cache_size_per_block  // per block output cache.
-        + sizeof(int)  // per block output counter.
-        + sizeof(int);  // per block output start index.
+        pattern_size + sizeof(int) * output_cache_size_per_block // per block output cache.
+        + sizeof(int)                                            // per block output counter.
+        + sizeof(int);                                           // per block output start index.
 
     int num_blocks = ceil_div(
         text_length - (pattern_length - 1),
-        block_size * (match_length_per_thread - (pattern_length - 1))
-    );
+        block_size * (match_length_per_thread - (pattern_length - 1)));
 
     timer_start("Performing KMP on the GPU");
     KMP_search_shmem_kernel<<<num_blocks, block_size, shared_memory_size>>>(
         text_device, text_length, pattern_device, pattern_length,
-        output_device, output_cnt_device, max_output_cnt, fail_device
-    );
+        output_device, output_cnt_device, max_output_cnt);
     THROW_IF_ERROR(cudaDeviceSynchronize());
     timer_stop();
 
     timer_start("Copying output memory to the CPU");
     int output_cnt;
     THROW_IF_ERROR(
-        cudaMemcpy(&output_cnt, output_cnt_device, sizeof(int), cudaMemcpyDeviceToHost)
-    );
-    output_cnt = min(output_cnt, max_output_cnt);  // This counter may overflow.
+        cudaMemcpy(&output_cnt, output_cnt_device, sizeof(int), cudaMemcpyDeviceToHost));
+    output_cnt = min(output_cnt, max_output_cnt); // This counter may overflow.
     THROW_IF_ERROR(cudaMemcpy(output, output_device, output_size, cudaMemcpyDeviceToHost));
     timer_stop();
 
@@ -203,7 +198,6 @@ int KMP_search_shmem(
     cudaFree(text_device);
     cudaFree(pattern_device);
     cudaFree(output_device);
-    cudaFree(fail_device);
     cudaFree(output_cnt_device);
     timer_stop();
     return output_cnt;
